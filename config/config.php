f84354867882da4bd6150d17cd509559b30e6d25
onglo <wertyedouard@gmail.com>
2016-12-28 14:57:26 +0000

    uploaded whole root directory to github
    ensures that the config.php is in the gig

---
+++ config/config.php
@@ -0,0 +1,37 @@
+<?php
+
+$dbUsername = "cl11-main-rh8";
+$dbPassword = "tWdUeUMNB";
+$dbServer = "localhost";
+
+// the secret key for reCAPTCHA
+$reCAPTCHA = "6Lc38A8UAAAAAGtxlsslgQ6suFCT-u0xZECx00LJ";
+
+// a function to encrypt a user's private key
+function encryptPrivate($privateKeyInput, $passwordInput, $userSaltInput) {
+
+  // hash the private key
+  $encryptedPrivate = md5($privateKeyInput.$passwordInput.$userSaltInput);
+
+  // return this value
+  return $encryptedPrivate;
+}
+
+// a function to encrypt passwords
+function encryptPassword($passwordToEncrypt, $userSaltInput) {
+
+  $encrypted = crypt($passwordToEncrypt.$userSaltInput);
+
+  // return this value
+  return $encrypted;
+
+}
+
+// a function to encrypt emails
+function encryptEmail($targetEmail) {
+    // encrypt the email and return it
+    $targetEmail = md5($targetEmail."W6zl5BGXAylZpOCA25El");
+    return $targetEmail;
+}
+
+?>
