0e2bebf4315373ef30a17c85eb5ce20ead1d4d37
onglo <wertyedouard@gmail.com>
2017-01-08 13:57:50 +0000

    added a file that is auto run every night that archives events that are no longer valid
    deletes events which happened yesterday!

---
+++ config/cleanUpEvents.php
@@ -0,0 +1,35 @@
+<?php
+
+include("/home/sites/edutix.com/config/config.php");
+
+// attempt to link to the db
+$link = mysqli_connect($dbServer, $dbUsername, $dbPassword, $dbUsername);
+
+// check if the connection was succesful
+if(mysqli_connect_error()) {
+  // kill the script
+  die();
+};
+
+// prepare a query that will move the old events from one table to the archive TokyoTyrantTable
+$moveQuery = "INSERT INTO `etonEventsArchive` SELECT * FROM `etonEvents` WHERE `eventDate` < CURDATE()";
+
+// prepare a query that will delete the events we just moved
+$deleteQuery = "DELETE FROM `etonEvents` WHERE `eventDate` < CURDATE()";
+
+// attept to execute the query
+if (mysqli_query($link, $moveQuery)) {
+
+    // attempt to delete the events from the previous table
+    if (mysqli_query($link, $deleteQuery)) {
+        echo "success";
+    }
+    else {
+        die();
+    }
+}
+else {
+    die();
+}
+
+?>
