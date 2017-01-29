3680000135eae17cea071828bd7db24356c799bb
onglo <wertyedouard@gmail.com>
2017-01-29 20:29:34 +0000

    notifications for editing event now working!

--- public_html/eton/mobile/scripts/updateEvent.php
+++ public_html/eton/mobile/scripts/updateEvent.php
@@ -12,13 +12,16 @@ if(mysqli_connect_error()) {
 };
 
 // prepare a query to fetch the salt for that event
-$query = "SELECT `salt` FROM `".mysqli_real_escape_string($link, $_POST["db"])."` WHERE `id` = ".mysqli_real_escape_string($link, $_POST["eventID"])." LIMIT 1";
+$query = "SELECT `salt`,`idOfPeopleGoing` FROM `".mysqli_real_escape_string($link, $_POST["db"])."` WHERE `id` = ".mysqli_real_escape_string($link, $_POST["eventID"])." LIMIT 1";
 
 // attempt the query
 if ($result = mysqli_query($link, $query)) {
+
     // save this as the salt
-    $salt = mysqli_fetch_array($result);
-    $salt = $salt["salt"];
+    $data = mysqli_fetch_array($result);
+    $salt = $data["salt"];
+
+    $peopleGoing = $data["idOfPeopleGoing"];
 }
 else {
     die();
@@ -39,6 +42,54 @@ $userQuery = "UPDATE `".mysqli_real_escape_string($link, $_POST["db"])."` SET `e
 
 // attempt the query
 if (mysqli_query($link, $userQuery)) {
+
+    // check if the user wants to email all of the people going to the event
+    if ($_POST["email"] == "true") {
+
+        // get the list of people going to the event
+        $listOfPeople = str_replace(".", ",", $peopleGoing);
+
+        // remove the first comma
+        $listOfPeople = substr($listOfPeople, 1);
+
+        // prepare a query that will fetch all of these user's emails
+        $emailQuery = "SELECT `email` FROM `etonUsers` WHERE `id` IN (".mysqli_real_escape_string($link, $listOfPeople).")";
+
+        // format the message we will give to the users
+        $arrayOfChanges = explode("$$$", $_POST["changed"]);
+
+        // initialise the message var
+        $messageChange = "";
+
+        // loop through each of these and add them to a big string
+        foreach ($arrayOfChanges as $change) {
+            if ($change != "") {
+                $messageChange .= $change.", ";
+            }
+        }
+
+        // remove the final comma
+        $messageChange = substr($messageChange, 0 , -2);
+
+        // add a full stop
+        $messageChange .= ".";
+
+        // get the data from this query
+        if ($emails = mysqli_query($link, $emailQuery)) {
+
+            // loop through each email
+            while ($userEmail = mysqli_fetch_array($emails)) {
+
+                // prepare an email that we will send to the user
+                $message = "Hello,\r\n\r\nJust a quick email to let you know that an event your going to, ".$_POST["eventTitle"].", has changed some things about the event: ".$messageChange."\r\n\r\nMany Thanks,\r\nThe Edutix Team";
+
+                // mail the message
+                mail(decryptEmail($userEmail["email"]), $_POST["eventTitle"]." has been updated", $message, "'From:hello@Edutix.com' . '\r\n'");
+
+            }
+        }
+    }
+
     echo "success";
 }
 else {
